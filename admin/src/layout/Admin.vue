e5363c0030c59e13724c31da06b64fdbc0c97427
SolalDR <solal.dussout-revel@hotmail.fr>
2019-03-27 19:14:22 +0100

    first commit

---
+++ admin/src/layout/Admin.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="page dashboard-page md-primary">
+    <md-app>
+      <md-app-toolbar class="md-primary">
+        <span class="md-title">{{ title }}</span>
+      </md-app-toolbar>
+
+      <md-app-drawer md-permanent="full">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          Menu
+        </md-toolbar>
+
+        <md-list>
+          <md-list-item>
+            <md-icon>videogame_asset</md-icon>
+            <span class="md-list-item-text">Rooms</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>business</md-icon>
+            <span class="md-list-item-text">Models</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>settings</md-icon>
+            <span class="md-list-item-text">Config</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>av_timer</md-icon>
+            <span class="md-list-item-text">Logs</span>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+
+      <md-app-content>
+        <slot/>
+      </md-app-content>
+    </md-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  props: {
+    title: {
+      type: String,
+      default: 'Admin',
+    },
+  },
+  data() {
+    return {
+      sending: false,
+      form: {
+        username: {
+          value: '',
+        },
+        password: {
+          value: '',
+        },
+      },
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.login {
+  margin: auto;
+}
+</style>
---
+++ admin/src/pages/Dashboard.vue
@@ -0,0 +1,54 @@
+<template>
+  <admin title="Dashboard">
+    <span class="md-title">Bienvenue dans votre dashboard</span>
+  </admin>
+</template>
+
+<script>
+import Admin from './../layout/Admin';
+
+export default {
+  name: 'Dashboard',
+
+  components: {
+    Admin,
+  },
+
+  data() {
+    return {
+      sending: false,
+      form: {
+        username: {
+          value: '',
+        },
+        password: {
+          value: '',
+        },
+      },
+    };
+  },
+
+  methods: {
+    validateUser() {
+      this.sending = true;
+      setTimeout(() => {
+        this.$router.push('/dashboard');
+      });
+      return true;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login-page {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.login {
+  margin: auto;
+}
+</style>
