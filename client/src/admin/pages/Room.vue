e714f0a4296e94c01114210a4a80870632b284b5
eronne <erwann.letue@gmail.com>
2019-04-02 16:57:59 +0200

    Added overlay for different room infos

--- client/src/admin/pages/Room.vue
+++ client/src/admin/pages/Room.vue
@@ -1,69 +1,73 @@
 <template>
   <div class="admin-room">
     <h1 class="admin-room__title md-display-1" v-if="room">Room: {{ room.name }}</h1>
-    <div class="header">
-      <md-tabs md-sync-route>
-        <md-tab id="tab-home" md-label="Overview" @click="component = 'overview'">
+
+    <header>
+      <md-tabs>
+        <template slot="md-tab" slot-scope="{ tab }">
+          {{ tab.label }} <i class="badge" v-if="tab.data.badge">{{ tab.data.badge }}</i>
+        </template>
+
+        <md-tab id="tab-overview" md-label="Overview">
+          <md-card class="md-size-50">
+            <md-card-header>
+              <div class="md-title">Overview</div>
+            </md-card-header>
+
+            <md-card-content>
+              <p>Créer à : {{ new Date(room.createdAt).toLocaleString() }}</p>
+              <p v-if="room.game">
+                Début du jeu : {{ new Date(room.game.startedAt).toLocaleString() }}
+              </p>
+              <p v-if="room.game && room.game.endedAt">
+                Terminé à : {{ new Date(room.game.endedAt).toLocaleString() }}
+              </p>
+              <p v-if="room.game && !room.game.endedAt">
+                Temps écoulé : {{  Math.floor((Date.now() - room.game.startedAt)*0.001) + 's' }}
+              </p>
+            </md-card-content>
+          </md-card>
         </md-tab>
-        <md-tab id="tab-pages" md-label="History" @click="component = 'history'">
+
+        <md-tab id="tab-metrics" md-label="Metrics">
+          <md-card>
+            <md-card-header>
+              <div class="md-title">Metrics</div>
+            </md-card-header>
+
+            <md-card-content class="metrics">
+              <div v-for="(metric, i) in metrics" :key="i" class="metric">
+                <p>{{ metric.name }}</p>
+                <md-progress-bar md-mode="buffer" :md-buffer="100" :md-value="metric.value"></md-progress-bar>
+              </div>
+            </md-card-content>
+          </md-card>
         </md-tab>
-        <md-tab id="tab-posts" md-label="Map" @click="component = 'map'">
+
+        <md-tab id="tab-logs" md-label="Logs" :md-template-data="{ badge: unreadedLogs }" @click="unreadedLogs = 0">
+          <p>Logs content</p>
         </md-tab>
       </md-tabs>
-    </div>
-    <div>
+    </header>
 
-      <md-card class="md-size-50">
-        <md-card-header>
-          <div class="md-title">Overview</div>
-        </md-card-header>
-        <md-card-content>
-          <p>Créer à : {{ new Date(room.createdAt).toLocaleString() }}</p>
-          <p v-if="room.game">
-            Début du jeu : {{ new Date(room.game.startedAt).toLocaleString() }}
-          </p>
-          <p v-if="room.game && room.game.endedAt">
-            Terminé à : {{ new Date(room.game.endedAt).toLocaleString() }}
-          </p>
-          <p v-if="room.game && !room.game.endedAt">
-            Temps écoulé : {{  Math.floor((Date.now() - room.game.startedAt)*0.001) + 's' }}
-          </p>
-        </md-card-content>
-      </md-card>
+    <md-card>
+      <md-card-header>
+        <div class="md-title">Grille</div>
+      </md-card-header>
 
-      <md-card>
-        <md-card-header>
-          <div class="md-title">Metrics</div>
-        </md-card-header>
-
-        <md-card-content class="metrics">
-          <div v-for="(metric, i) in metrics" :key="i" class="metric">
-            <p>{{ metric.name }}</p>
-            <md-progress-bar md-mode="buffer" :md-buffer="100" :md-value="metric.value"></md-progress-bar>
-          </div>
-        </md-card-content>
-      </md-card>
-
-
-      <md-card>
-        <md-card-header>
-          <div class="md-title">Grille</div>
-        </md-card-header>
-
-        <md-card-content>
-          <grid-component
-            :size="[32, 32]"
-            :cells="entities"
-            @clickCell="onClickCell($event)"
-            />
-        </md-card-content>
-      </md-card>
-    </div>
+      <md-card-content>
+        <grid-component
+          :size="[32, 32]"
+          :cells="entities"
+          @clickCell="onClickCell($event)"
+          />
+      </md-card-content>
+    </md-card>
   </div>
 </template>
 
 <script>
-
+// TODO: Add templates in components
 // import Overview from './rooms/Overview.vue';
 // import Map from './rooms/Map.vue';
 // import History from './rooms/History.vue';
@@ -81,10 +85,11 @@ export default {
 
   data() {
     return {
-      room: null,
-      metrics: [],
       entities: new Array(32 * 32).fill(null),
-      component: 'overview',
+      logs: ['Log 1', 'Log 2', 'Log 1', 'Log 2', 'Log 1', 'Log 2', 'Log 1', 'Log 2', 'Log 1', 'Log 2', 'Log 1', 'Log 2'],
+      unreadedLogs: 0,
+      metrics: [],
+      room: null,
     };
   },
 
@@ -104,7 +109,7 @@ export default {
       color: '#CCC',
     };
 
-    console.log(this.entities);
+    this.unreadedLogs = this.logs.length
   },
 
   methods: {
@@ -142,8 +147,29 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .admin-room {
+  header {
+    .badge {
+      // TODO: Add scss variables
+      width: 19px;
+      height: 19px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      top: 2px;
+      right: 2px;
+      background: red;
+      border-radius: 50%;
+      color: white;
+      font-size: 10px;
+      font-style: normal;
+      font-weight: 600;
+      letter-spacing: -.05em;
+    }
+  }
+
   .md-card {
     margin-bottom: 32px;
   }
@@ -153,10 +179,6 @@ export default {
   }
   .md-tabs-navigation {
     margin-bottom: 32px;
-  }
-
-  .header {
-    display: none;
   }
 
   .metrics {
