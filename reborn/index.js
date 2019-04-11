1043b79a460e10e160b9ce8ad6c13229df1625fc
Solal Dussout-Revel <solal.dussout-revel@hotmail.fr>
2019-04-11 12:31:50 +0200

    Add exports

--- reborn/index.js
+++ reborn/index.js
@@ -1,13 +1,22 @@
-// Todo update list
+// Timeline
 export {default as Timeline} from "./timeline/Timeline";
+export {default as TimelineEvent} from "./timeline/Event";
 
+// Role
 export {default as CityRole} from "./role/City";
 export {default as NatureRole} from "./role/Nature";
+export {default as Role} from "./role/Role";
 
+// Metric
+export {default as Metrics} from "./metric";
+export {default as Metric} from "./metric/Metric";
+
+// Entity
 export {default as EntityModel} from "./entity/Model";
 export {default as EntityState} from "./entity/State";
 
+// Main
 export {default as Game} from "./Game";
 export {default as World} from "./World";
-
-
+export {default as Player} from "./Player";
+export {default as Grid} from "./Grid";
