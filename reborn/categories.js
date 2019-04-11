bb7cd0243c46c120afceff1e3c75bc4692e49e17
Solal Dussout-Revel <solal.dussout-revel@hotmail.fr>
2019-04-11 17:51:53 +0200

    remove docs and add skills

--- reborn/categories.js
+++ reborn/categories.js
@@ -1,15 +1,58 @@
 export default {
+
+  alimentation: {
+    order: 1,
+    slug: 'alimentation',
+    color: '#ff6e6e',
+    name: 'Alimentation',
+    role: 'city',
+  },
+
+  energy: {
+    order: 2,
+    slug: 'energy',
+    color: '#ffea76',
+    name: 'Energie',
+    role: 'city',
+  },
+
   satisfaction: {
+    order: 3,
     slug: 'satisfaction',
-    color: '#FF0000',
+    color: '#6ea3ff',
     name: 'Satisfaction',
     role: 'city',
   },
 
+  purity: {
+    order: 4,
+    slug: 'purity',
+    color: '#abffa9',
+    name: 'Pureté',
+    role: 'city',
+  },
+
   vegegation: {
+    order: 1,
     slug: 'vegegation',
-    color: '#FF0000',
+    color: '#abffa9',
     name: 'Végegation',
     role: 'nature',
-  }
+  },
+
+  rock: {
+    order: 2,
+    slug: 'rock',
+    color: '#27293f',
+    name: 'Minerai',
+    role: 'nature',
+  },
+
+  skill: {
+    order: 3,
+    slug: 'skill',
+    color: '#f90',
+    name: 'Pouvoir',
+    role: 'nature',
+  },
 }
---
+++ reborn/entity/models/farm.js
@@ -0,0 +1,39 @@
+export default {
+  slug: "farm",
+  category: "satisfaction",
+  color: "#FF0000",
+  name: "Logement",
+  states: {
+    creation: {
+      duration: 1000,
+      enterModifiers: [
+        {
+          name: 'pollution',
+          value: 1
+        },
+        {
+          name: 'money',
+          value: -1000,
+          checkConstraint: true
+        }
+      ]
+    },
+    mounted: {
+      recurModifiers: [
+        {
+          name: 'pollution',
+          value: 1
+        }
+      ]
+    },
+    destruction: {
+      leaveModifiers: [
+        {
+          name: 'money',
+          value: 100
+        }
+      ]
+    }
+  }
+}
+
