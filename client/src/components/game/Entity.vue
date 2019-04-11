516d1efa58e2410b2a804d55699c9455f1ca47ec
eronne <erwann.letue@gmail.com>
2019-04-11 16:46:09 +0200

    Added game interface components

---
+++ client/src/components/game/Entity.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="entity" @click="setCurrentEntity">
+    Entity
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'entity',
+  props: {
+    entity: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    setCurrentEntity() {
+      console.log('Set current Entity');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .entity {
+    $size: 80px;
+
+    overflow: hidden;
+    @include useFlex();
+    width: $size;
+    height: $size;
+    border-radius: 50%;
+    background-color: rgba(getColor(basics, white), .7);
+  }
+</style>
--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -1,15 +1,61 @@
 <template>
   <main class="game">
     <scene/>
+
+    <div class="game__interface">
+      <div class="metrics">
+        <!-- TODO: Bind metrics -->
+        <metric v-for="(metric, index) in 2" :key="index" :metric="{}"/>
+      </div>
+
+      <div class="years-counter">
+        <!-- TODO: Bind currentYear value -->
+        <years-counter :currentYear="75"/>
+      </div>
+
+      <div class="indicators">
+        <!-- TODO: Bind indicators -->
+        <indicator v-for="(indicator, index) in 2" :key="index" :indicator="{}"/>
+      </div>
+
+      <div class="entities">
+        <!-- TODO: Bind entities -->
+        <entity v-for="(entity, index) in 4" :key="index" :entity="{}"/>
+      </div>
+
+      <div @click="showSettings = true" class="settings-cta">
+        Settings
+      </div>
+
+      <transition name="settings">
+        <settings v-if="showSettings" @closeSettings="showSettings = false"/>
+      </transition>
+    </div>
   </main>
 </template>
 
 <script>
-import Scene from '../components/Scene.vue';
+import Metric from '../components/game/Metric.vue';
+import Entity from '../components/game/Entity.vue';
+import Scene from '../components/game/Scene.vue';
+import Indicator from '../components/game/Indicator.vue';
+import Settings from '../components/game/Settings.vue';
+import YearsCounter from '../components/game/YearsCounter.vue';
 
 export default {
   components: {
+    YearsCounter,
+    Settings,
+    Indicator,
     Scene,
+    Entity,
+    Metric,
+  },
+
+  data() {
+    return {
+      showSettings: false,
+    };
   },
 
   sockets: {
@@ -47,6 +93,16 @@ export default {
       this.$socket.emit('room:retrieve', this.$store.state.roomId);
     }
   },
+
+  methods: {
+    /*
+    * TODO: Add shortcutsa
+    * F1, F2, F3, F4 --> select currentCategory
+    * 1, 2, 3, 4 --> select currentEntity
+    * Escape: Open settings
+    * F: Fullscreen
+    * */
+  },
 };
 </script>
 
@@ -55,5 +111,56 @@ export default {
   width: 100%;
   height: 100vh;
   overflow: hidden;
+
+  &__interface {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+
+    & > div {
+      position: absolute;
+    }
+
+    .metrics {
+      top: 0;
+      left: 0;
+    }
+
+    .years-counter {
+      top: 0;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+
+    .indicators {
+      top: 0;
+      right: 0;
+    }
+
+    .entities {
+      @include useFlex(space-between);
+      bottom: 0;
+      left: 50%;
+      transform: translateX(-50%);
+      padding: 10px;
+      width: 400px;
+      border-radius: 50px;
+      background-color: rgba(getColor(basics, white), .5);
+    }
+
+    .settings-cta {
+      $size: 80px;
+
+      bottom: 0;
+      right: 0;
+      @include useFlex();
+      width: $size;
+      height: $size;
+      background-color: getColor(basics, white);
+      border-radius: 50%;
+    }
+  }
 }
 </style>
