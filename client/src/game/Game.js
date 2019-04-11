cfdd323e95d611be216f6f7087abd43e2b91da1b
Solal Dussout-Revel <solal.dussout-revel@hotmail.fr>
2019-04-11 17:36:42 +0200

    add player on reborn

--- client/src/game/Game.js
+++ client/src/game/Game.js
@@ -1,9 +1,14 @@
 import * as Reborn from '../../../reborn';
+import store from '../services/store';
 
 export default class Game extends Reborn.Game {
-  constructor({ players = null } = {}) {
+  constructor({
+    players = null,
+  } = {}) {
     super({ players });
+  }
 
-    console.log(this);
+  get player() {
+    return this.players.find(p => p.id === store.state.playerId);
   }
 }
