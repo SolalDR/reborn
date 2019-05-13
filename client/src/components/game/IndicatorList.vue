71faca526652df3ae3009ca1c143b70f1799e49a
eronne <erwann.letue@gmail.com>
2019-05-13 17:02:15 +0200

    Added icons for indicator-list

--- client/src/components/game/IndicatorList.vue
+++ client/src/components/game/IndicatorList.vue
@@ -3,21 +3,60 @@
     <div v-for="(indicator, index) in list"
       :key="index"
       class="indicator">
-      <label>{{ indicator.name }}</label>
+      <img :src="icons[indicator.slug]" :alt="indicator.name">
       <span>{{ indicator.value }}</span>
     </div>
 
-    <div @click="$emit('showSettings', true)" class="settings-cta">Settings</div>
+    <img @click="$emit('showSettings', true)" :src="icons.settings" alt="Paramètres" class="indicator">
   </div>
 </template>
 
 <script>
+import money from '../../assets/icons/game/common/money.svg';
+import population from '../../assets/icons/game/common/population.svg';
+import settings from '../../assets/icons/game/common/settings.svg';
+
 export default {
+  name: 'indicator-list',
   props: {
     list: {
       type: Array,
       required: true,
     },
   },
+  data() {
+    return {
+      icons: {
+        money,
+        population,
+        settings
+      }
+    };
+  },
 };
 </script>
+
+<style lang="scss">
+  .indicator-list {
+    @include useFlex();
+
+    .indicator {
+      @include useFlex();
+      margin-right: 3rem;
+
+      &:last-child,
+      &:last-child img {
+        margin-right: 0;
+      }
+
+      img {
+        margin-right: 1rem;
+      }
+
+      span {
+        @include fontSize(18);
+        font-family: "DrukText-Medium";
+      }
+    }
+  }
+</style>
