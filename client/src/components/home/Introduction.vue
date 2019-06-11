f4f31e354c49878e76a831db730662fa0a5cbbb8
eronne <erwann.letue@gmail.com>
2019-06-11 17:31:09 +0200

    Added gifs in introduction

--- client/src/components/home/Introduction.vue
+++ client/src/components/home/Introduction.vue
@@ -1,6 +1,11 @@
 <template>
   <overlay :appear="false" :is-transparent="false" :bottom-waves="true">
     <template #default>
+      <div class="introduction__gifs">
+        <img :src="gifs[currentIndex]" :alt="`Gif ${currentIndex + 1}`" class="diffuse">
+        <img :src="gifs[currentIndex]" :alt="`Gif ${currentIndex + 1}`" class="mask">
+      </div>
+
       <div class="introduction__texts">
         <p
           v-for="(text, index) in texts[currentIndex]"
@@ -17,8 +22,10 @@
 </template>
 
 <script>
-import texts from '@/contents/home/introduction';
 import Overlay from '../global/Overlay';
+
+import gifs from '@/assets/img/home/gifs';
+import texts from '@/contents/home/introduction';
 
 export default {
   name: 'introduction',
@@ -31,6 +38,7 @@ export default {
   data() {
     return {
       currentIndex: 0,
+      gifs: gifs,
       texts,
     };
   },
@@ -55,9 +63,55 @@ export default {
 </script>
 
 <style lang="scss">
+  $height: 32rem;
   $animation-duration: 8s;
 
   .introduction {
+    &__gifs {
+      margin-top: -20vh;
+      transform-origin: center center;
+      width: 100%;
+      height: $height;
+      background-color: rgb(122, 226, 182);
+      animation: gif-animation $animation-duration cubic-bezier(0, .01, 0, 1) infinite;
+
+      @keyframes gif-animation {
+        0% {
+          opacity: 0;
+          transform: translateY(6vh);
+        }
+        15% {
+          opacity: 1;
+          transform: translateY(0);
+        }
+        85% {
+          opacity: 1;
+          transform: translateY(0);
+        }
+        100% {
+          opacity: 0;
+          transform: translateY(-6vh);
+        }
+      }
+
+      img {
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        height: $height !important;
+      }
+
+      .diffuse {
+        filter: invert(100%);
+        mix-blend-mode: screen;
+      }
+
+      .mask {
+        mix-blend-mode: multiply;
+      }
+    }
+
     &__texts {
       animation: scaleContent $animation-duration linear infinite;
 
